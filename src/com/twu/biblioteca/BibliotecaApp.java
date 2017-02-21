b6c2ee9fde40cf3d669b837b850b0d5f8f58579c
yibwang <yibwang@thoughtworks.com>
2017-02-22 02:17:33 +0800

    Add list book function.

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,44 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.welcome();
+        bibliotecaApp.listBooks();
+    }
+
+    public void welcome() {
+        System.out.println("Welcome!");
+    }
+
+    public void listBooks() throws IOException {
+        System.out.println("Book List:");
+        List<String> books = getBookList("library.txt");
+        for(String book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public List<String> getBookList(String filepath) throws IOException {
+        File file = new File(filepath);
+        FileInputStream stream = new FileInputStream(file);
+        InputStreamReader reader = new InputStreamReader(stream);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        List<String> books = new ArrayList<String>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            books.add(line);
+        }
+        return books;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,20 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void should_get_book_list_in_library_file() throws IOException {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        List<String> books = bibliotecaApp.getBookList("test/com/twu/biblioteca/test_lib.txt");
+        assertEquals(2, books.size());
+        assertEquals("Refactor", books.get(0));
+        assertEquals("TDD", books.get(1));
     }
 }
